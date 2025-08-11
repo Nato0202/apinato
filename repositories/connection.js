55980a7b446f6d600a9b3d42cb01b71de9e8eb40
Nato0202 <nrenan2007@gmail.com>
2025-08-11 09:43:48 -0300

    Update connection.js

--- repositories/connection.js
+++ repositories/connection.js
@@ -4,7 +4,7 @@ const connection = await mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '1234',
-    database: 'atividade'
+    database: 'api'
 })
 
 export {connection};
